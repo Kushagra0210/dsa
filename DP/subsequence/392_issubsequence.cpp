6188ab810da9549d99173d24702f9376b1e0320a
Kushagra Saxena <kushagrasaxena0210@gmail.com>
2026-07-18 18:06:20 +0530

    392issubsequence

---
+++ DP/subsequence/392_issubsequence.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        int i=0;
+        int j=0;
+        if(n>m) return false;
+        while(i<n && j<m ){
+            if(s[i]==t[j])i++;
+            j++;
+        }
+        return i==n;
+    }
+};
